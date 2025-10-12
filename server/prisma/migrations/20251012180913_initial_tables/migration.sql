361b37d6f41e3f9c3d00e51fffdca8542166ddbe
Xaidel <abechuelak@gmail.com>
2025-10-13 02:10:20 +0800

    feat: initial schema

---
+++ server/prisma/migrations/20251012180913_initial_tables/migration.sql
@@ -0,0 +1,108 @@
+-- CreateEnum
+CREATE TYPE "GENDER" AS ENUM ('FEMALE', 'MALE');
+
+-- CreateEnum
+CREATE TYPE "PaymentMethod" AS ENUM ('CASH', 'GCASH');
+
+-- CreateEnum
+CREATE TYPE "SCOPE" AS ENUM ('REGULAR', 'WALKIN', 'STUDENT');
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Goer" (
+    "id" SERIAL NOT NULL,
+    "full_name" TEXT NOT NULL,
+    "isStudent" BOOLEAN NOT NULL,
+    "gender" "GENDER" NOT NULL,
+    "birth_date" TIMESTAMP(3) NOT NULL,
+    "phone" TEXT,
+    "email" TEXT,
+
+    CONSTRAINT "Goer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Member" (
+    "id" SERIAL NOT NULL,
+    "goer_id" INTEGER NOT NULL,
+    "account_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Member_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Walkin" (
+    "id" SERIAL NOT NULL,
+    "geor_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Walkin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Attendance" (
+    "id" SERIAL NOT NULL,
+    "check_in" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "check_out" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Attendance_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Membership" (
+    "id" SERIAL NOT NULL,
+    "member_id" INTEGER NOT NULL,
+    "months" INTEGER NOT NULL,
+    "start_date" TIMESTAMP(3) NOT NULL,
+    "end_date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Membership_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Payment" (
+    "id" SERIAL NOT NULL,
+    "goer_id" INTEGER NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "payment_method" "PaymentMethod" NOT NULL,
+    "time" TIMESTAMP(3) NOT NULL,
+    "membershipId" INTEGER,
+
+    CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Promo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "discount" INTEGER NOT NULL,
+    "start_date" TEXT NOT NULL,
+    "end_date" TEXT NOT NULL,
+    "scope" "SCOPE"[],
+
+    CONSTRAINT "Promo_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Member" ADD CONSTRAINT "Member_goer_id_fkey" FOREIGN KEY ("goer_id") REFERENCES "Goer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Member" ADD CONSTRAINT "Member_account_id_fkey" FOREIGN KEY ("account_id") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Walkin" ADD CONSTRAINT "Walkin_geor_id_fkey" FOREIGN KEY ("geor_id") REFERENCES "Goer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Membership" ADD CONSTRAINT "Membership_member_id_fkey" FOREIGN KEY ("member_id") REFERENCES "Member"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_membershipId_fkey" FOREIGN KEY ("membershipId") REFERENCES "Membership"("id") ON DELETE SET NULL ON UPDATE CASCADE;
