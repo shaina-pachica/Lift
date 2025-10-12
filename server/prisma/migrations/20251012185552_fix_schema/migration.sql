bdd59d93162dbca48cd8c8fa519c45f0735022bf
Xaidel <abechuelak@gmail.com>
2025-10-13 02:56:14 +0800

    fix: schema

--- server/prisma/migrations/20251012185552_fix_schema/migration.sql
+++ server/prisma/migrations/20251012185552_fix_schema/migration.sql
@@ -52,7 +52,7 @@ CREATE TABLE "Walkin" (
 CREATE TABLE "Attendance" (
     "id" SERIAL NOT NULL,
     "check_in" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "check_out" TIMESTAMP(3) NOT NULL,
+    "check_out" TIMESTAMP(3),
     "goer_id" INTEGER NOT NULL,
 
     CONSTRAINT "Attendance_pkey" PRIMARY KEY ("id")
@@ -72,8 +72,8 @@ CREATE TABLE "Membership" (
 -- CreateTable
 CREATE TABLE "Payment" (
     "id" SERIAL NOT NULL,
-    "goer_id" INTEGER NOT NULL,
-    "amount" INTEGER NOT NULL,
+    "goerId" INTEGER NOT NULL,
+    "amount" DECIMAL(10,2) NOT NULL,
     "payment_method" "PaymentMethod" NOT NULL,
     "time" TIMESTAMP(3) NOT NULL,
     "membershipId" INTEGER,
@@ -87,12 +87,36 @@ CREATE TABLE "Promo" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "discount" INTEGER NOT NULL,
-    "start_date" TEXT NOT NULL,
-    "end_date" TEXT NOT NULL,
+    "start_date" TIMESTAMP(3) NOT NULL,
+    "end_date" TIMESTAMP(3) NOT NULL,
     "scope" "SCOPE"[],
 
     CONSTRAINT "Promo_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE INDEX "Member_goer_id_idx" ON "Member"("goer_id");
+
+-- CreateIndex
+CREATE INDEX "Member_account_id_idx" ON "Member"("account_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Member_goer_id_account_id_key" ON "Member"("goer_id", "account_id");
+
+-- CreateIndex
+CREATE INDEX "Walkin_goer_id_idx" ON "Walkin"("goer_id");
+
+-- CreateIndex
+CREATE INDEX "Attendance_goer_id_idx" ON "Attendance"("goer_id");
+
+-- CreateIndex
+CREATE INDEX "Payment_goerId_idx" ON "Payment"("goerId");
+
+-- CreateIndex
+CREATE INDEX "Payment_membershipId_idx" ON "Payment"("membershipId");
+
+-- CreateIndex
+CREATE INDEX "Payment_promoId_idx" ON "Payment"("promoId");
 
 -- AddForeignKey
 ALTER TABLE "Member" ADD CONSTRAINT "Member_goer_id_fkey" FOREIGN KEY ("goer_id") REFERENCES "Goer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -114,3 +138,6 @@ ALTER TABLE "Payment" ADD CONSTRAINT "Payment_membershipId_fkey" FOREIGN KEY ("m
 
 -- AddForeignKey
 ALTER TABLE "Payment" ADD CONSTRAINT "Payment_promoId_fkey" FOREIGN KEY ("promoId") REFERENCES "Promo"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_goerId_fkey" FOREIGN KEY ("goerId") REFERENCES "Goer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
